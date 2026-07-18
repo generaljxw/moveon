0f8a63d37a3497d0bb15fe5cf2785e9a976d2895
MoveOn Developer <developer@moveon.app>
2026-07-18 11:37:49 +0800

    chore: set Chinese window title and UTF-8 compile support

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -26,8 +26,10 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
 
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
-  Win32Window::Size size(1280, 720);
-  if (!window.Create(L"moveon", origin, size)) {
+  // 桌面端起始窗口尺寸 960×680，可按需调整
+  Win32Window::Size size(960, 680);
+  // 窗口标题设置为中文应用名
+  if (!window.Create(L"动起来 - MoveOn", origin, size)) {
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
